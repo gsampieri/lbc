87e9c66d984f44530b7e3bc0c1198cbef2adcb0d
Gilles SAMPIERI <gilles@sampieri.net>
2023-03-01 17:44:40 +0100

    add: tableVIew

--- lbc/UI/home/HomeViewController.swift
+++ lbc/UI/home/HomeViewController.swift
@@ -11,7 +11,42 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Advertisements"
+        makeTableView()
+    }
+    
+    // UI
+    private func makeTableView() {
+        let advertisementsTableView = UITableView()
+        advertisementsTableView.delegate = self
+        advertisementsTableView.dataSource = self
+        
+        view.addSubview(advertisementsTableView)
+        
+        advertisementsTableView.translatesAutoresizingMaskIntoConstraints = false
+        advertisementsTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        advertisementsTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        advertisementsTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        advertisementsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        advertisementsTableView.register(AdvertisementTableViewCell.self, forCellReuseIdentifier: "advertisementCell")
+
     }
 }
 
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // navigationController.push
+    }
+}
